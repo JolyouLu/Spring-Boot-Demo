f60d0e86e16a4c89bb93e4561a6ba8ec23d2454a
jolyouLu <1307633126@qq.com>
2021-10-11 13:38:44 +0800

    Redis工具类

--- redis_demo/src/main/java/top/jolyoulu/springboot_redis/utils/RedisUtils.java
+++ redis_demo/src/main/java/top/jolyoulu/springboot_redis/utils/RedisUtils.java
@@ -2,6 +2,7 @@ package top.jolyoulu.springboot_redis.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,102 @@ public class RedisUtils {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    /**
+     * 缓存基本的对象
+     * @param key 缓存的键
+     * @param value 缓存的值
+     */
+    public <T> void set(final String key,final T value){
+        redisTemplate.opsForValue().set(key,value);
+    }
+
+    /**
+     * 缓存一个基本对象并且设置过期时间
+     * @param key 缓存的键
+     * @param value 缓存的值
+     * @param timeout 超时时间（默认秒）
+     * @return true/false
+     */
+    public <T> void set(final String key,final T value,final long timeout){
+        this.set(key,value,timeout,TimeUnit.SECONDS);
+    }
+
+    /**
+     * 缓存一个基本对象并且设置过期时间
+     * @param key 缓存的键
+     * @param value 缓存的值
+     * @param timeout 超时时间
+     * @param unit 时间类型(时分秒)
+     * @return true/false
+     */
+    public <T> void set(final String key,final T value,final long timeout,final TimeUnit unit){
+        redisTemplate.opsForValue().set(key,value,timeout,unit);
+    }
+
+    /**
+     * 获取缓存基本的对象
+     * @param key 缓存的键
+     * @return 缓存的值
+     */
+    public <T> T get(final String key){
+        ValueOperations<String,T> operations = redisTemplate.opsForValue();
+        return operations.get(key);
+    }
+
+    /**
+     * 监测缓存是否存在指定key
+     * @param key 缓存的键
+     * @return
+     */
+    public boolean exists(final String key){
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * <key,value>方式
+     * 删除指定缓存对象
+     * @param key 缓存的键
+     * @return true/false
+     */
+    public <T> boolean del(final String key){
+        return redisTemplate.delete(key);
+    }
+
+    /**
+     * <key,Map<key,value>>方式
+     * 往缓存map中保存对象
+     * @param mapName map名称
+     * @param key 缓存的键
+     * @param value 缓存的值
+     * @param <T>
+     */
+    public <T> void hset(final String mapName,final String key,final T value){
+        redisTemplate.opsForHash().put(mapName,key,value);
+    }
+
+    /**
+     * <key,Map<key,value>>方式
+     * 获取缓存Map中的对象
+     * @param mapName map名称
+     * @param key 缓存的键
+     * @param <T>
+     * @return
+     */
+    public <T> T hget(final String mapName,final String key){
+        return (T) redisTemplate.opsForHash().get(mapName,key);
+    }
+
+    /**
+     * <key,Map<key,value>>方式
+     * 删除缓存Map中的对象
+     * @param mapName map名称
+     * @param key 缓存的键
+     * @param <T>
+     */
+    public <T> void hdel(final String mapName,final String... key){
+        redisTemplate.opsForHash().delete(mapName,key);
+    }
 
     /**
      * 原子性递减1
