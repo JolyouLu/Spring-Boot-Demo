67ea8b63b98969848fa930932db2f7fb3a8a521b
JolyouLu <1307633126@qq.com>
2022-07-02 15:03:18 +0800

    高效消费消息

--- ali_canal_demo/src/main/java/top/jolyoulu/controller/TestHandlerController.java
+++ ali_canal_demo/src/main/java/top/jolyoulu/controller/TestHandlerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import top.jolyoulu.pipline.defhandler.AbstractMessageHandlerContextAdapter;
 import top.jolyoulu.pipline.DefaultMessagePipeline;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Author: JolyouLu
  * @Date: 2022/7/2 1:39
@@ -19,11 +22,15 @@ public class TestHandlerController {
     @Autowired
     private DefaultMessagePipeline defaultMessagePipeline;
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     private static String msg = "{\"data\":[{\t\t\"id\":\"2\",\t\t\"user_id\":\"666\",\t\t\"create_by\":\"bb\",\t\t\"create_time\":\"2022-05-25 13:14:55\",\t\t\"update_by\":\"\",\t\t\"update_time\":\"0000-00-00 00:00:00\",\t\t\"del_flag\":\"0\"\t},{\t\t\"id\":\"3\",\t\t\"user_id\":\"666\",\t\t\"create_by\":\"132\",\t\t\"create_time\":\"0000-00-00 00:00:00\",\t\t\"update_by\":\"\",\t\t\"update_time\":\"0000-00-00 00:00:00\",\t\t\"del_flag\":\"0\"\t}],\t\"database\":\"fenzhidao_product\",\t\"es\":1655191608000,\t\"id\":249544,\t\"isDdl\":false,\t\"mysqlType\":{\t\t\"id\":\"varchar(32)\",\t\t\"user_id\":\"varchar(32)\",\t\t\"create_by\":\"varchar(32)\",\t\t\"create_time\":\"datetime\",\t\t\"update_by\":\"varchar(32)\",\t\t\"update_time\":\"datetime\",\t\t\"del_flag\":\"tinyint(1) unsigned\"\t},\t\"old\":[{\t\t\"user_id\":\"cc\"\t},{\t\t\"user_id\":\"55\"\t}],\t\"pkNames\":[\"id\"],\t\"sql\":\"\",\t\"sqlType\":{\t\t\"id\":12,\t\t\"user_id\":12,\t\t\"create_by\":12,\t\t\"create_time\":93,\t\t\"update_by\":12,\t\t\"update_time\":93,\t\t\"del_flag\":-6\t},\t\"table\":\"fzd_a\",\t\"ts\":1655191608243,\t\"type\":\"UPDATE\"}";
 
     @GetMapping
     public void test() {
-        AbstractMessageHandlerContextAdapter ctx = defaultMessagePipeline.getCtx();
-        ctx.accept(ctx,msg);
+        executorService.submit(() ->{
+            AbstractMessageHandlerContextAdapter ctx = defaultMessagePipeline.getCtx();
+            ctx.accept(ctx,msg);
+        });
     }
 }
